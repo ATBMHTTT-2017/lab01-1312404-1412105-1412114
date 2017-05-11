11260a3bff1f8bf96263946cca3e354df432db04
1412105 <1412105@student.hcmus.edu.vn>
2017-05-12 00:31:47 +0700

    Edits Tables and Constrains script 1

--- Schemas/Constraints and Triggers.sql
+++ Schemas/Constraints and Triggers.sql
@@ -1,29 +1,27 @@
 -- Employee
 ALTER TABLE Employee
-ADD CONSTRAINT FK_BelongedDepartment FOREIGN KEY(Department) REFERENCES Department(DepartmentID)
-    -- Trigger
+ADD CONSTRAINT FK_BelongedDepartment FOREIGN KEY(Department, Branch) REFERENCES Department(DepartmentID, Branch);
 
 -- Branch
 ALTER TABLE Branch
-ADD CONSTRAINT FK_Manager FOREIGN KEY(BranchManager) REFERENCES Employee(EmployeeID)
+ADD CONSTRAINT FK_Manager FOREIGN KEY(BranchManager) REFERENCES Employee(EmployeeID);
 
 -- Department
 ALTER TABLE Department
-ADD CONSTRAINT FK_Chief FOREIGN KEY(DepartmentChief) REFERENCES Employee(EmployeeID)
+ADD CONSTRAINT FK_Chief FOREIGN KEY(DepartmentChief) REFERENCES Employee(EmployeeID);
 ALTER TABLE Department
-ADD CONSTRAINT FK_BelongedBranch FOREIGN KEY(Branch) REFERENCES Branch(BranchID)
+ADD CONSTRAINT FK_BelongedBranch FOREIGN KEY(Branch) REFERENCES Branch(BranchID);
 
 -- Project
-ALTER TABLE Project
-ADD CONSTRAINT FK_HoldingDepartment FOREIGN KEY(HoldingDepartment) REFERENCES Department(DepartmentID)
-    -- Trigger
+ALTER TABLE Projects
+ADD CONSTRAINT FK_HoldingDepartment FOREIGN KEY(HoldingDepartment, Branch) REFERENCES Department(DepartmentID, Branch);
 
 -- Expenditure
 ALTER TABLE Expenditure
-ADD CONSTRAINT FK_OfProject FOREIGN KEY(Project) REFERENCES Project(ProjectID)
+ADD CONSTRAINT FK_OfProject FOREIGN KEY(Projects) REFERENCES Projects(ProjectID);
 
 -- Assignment
 ALTER TABLE Assignment
-ADD CONSTRAINT FK_OfProject FOREIGN KEY(Project) REFERENCES Project(ProjectID)
+ADD CONSTRAINT FK_AssignmentOfProject FOREIGN KEY(Projects) REFERENCES Projects(ProjectID);
 ALTER TABLE Assignment
-ADD CONSTRAINT FK_HandlingEmployee FOREIGN KEY(Employee) REFERENCES Employee(EmployeeID)
+ADD CONSTRAINT FK_HandlingEmployee FOREIGN KEY(Employee) REFERENCES Employee(EmployeeID);
--- Schemas/Tables.sql
+++ Schemas/Tables.sql
@@ -3,58 +3,60 @@ GO
 USE ProjectManagement
 GO
 
-CREATE TABLE Employee
+CREATE TABLE Employee+
 (
     EmployeeID CHAR(5) PRIMARY KEY,
-    EmployeeName NVARCHAR(50),
-    EmployeeAddress NVARCHAR(100),
+    EmployeeName varchar2(50),
+    EmployeeAddress varchar2(100),
     PhoneNumber VARCHAR(20),
     Email VARCHAR(50),
     Department CHAR(4),
     Branch CHAR(3),
     Salary INT
-)
+);
 
 CREATE TABLE Branch
 (
     BranchID CHAR(3) PRIMARY KEY,
-    BranchName NVARCHAR(50),
+    BranchName varchar2(50),
     BranchManager CHAR(5)
-)
+);
 
 CREATE TABLE Department
 (
-    DepartmentID CHAR(4) PRIMARY KEY,
-    DepartmentName NVARCHAR(50),
+    DepartmentID CHAR(4),
+    DepartmentName varchar2(50),
     DepartmentChief CHAR(5),
     ElectedDate DATE,
     EmployeeQuantity INT,
-    Branch CHAR(3)
-)
+    Branch CHAR(3),
+    CONSTRAINT PK_Department PRIMARY KEY (DepartmentID, Branch)
+);
 
-CREATE TABLE Project
+CREATE TABLE Projects
 (
     ProjectID CHAR(6) PRIMARY KEY,
-    ProjectName NVARCHAR(100),
+    ProjectName varchar2(100),
     Fund INT,
     HoldingDepartment CHAR(4),
+    Branch CHAR(3),
     ProjectManager CHAR(5)
-)
+);
 
 CREATE TABLE Expenditure
 (
     ExpenditureID CHAR(7) PRIMARY KEY,
-    ExpenditureName NVARCHAR(100),
+    ExpenditureName varchar2(100),
     Amount INT,
-    Project CHAR(6)
-)
+    Projects CHAR(6)
+);
 
 CREATE TABLE Assignment
 (
-    Project CHAR(6),
+    Projects CHAR(6),
     Employee CHAR(5),
-    Position NVARCHAR(20),
+    Position varchar2(20),
     SuppliedFund INT,
 
-    CONSTRAINT PK_Assignment PRIMARY KEY(Project, Employee)
-)
+    CONSTRAINT PK_Assignment PRIMARY KEY(Projects, Employee)
+);
