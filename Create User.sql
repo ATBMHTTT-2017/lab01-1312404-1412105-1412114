4362887bca96b5cfeeae751c549a5d9aad424cba
1412105 <1412105@student.hcmus.edu.vn>
2017-05-20 17:28:38 +0700

    Create all user

---
+++ Create User.sql
@@ -0,0 +1,65 @@
+-- Create user for Emplyee
+grant create session to NV01 identified by NV01;
+grant create session to NV02 identified by NV02;
+grant create session to NV03 identified by NV03;
+grant create session to NV04 identified by NV04;
+grant create session to NV05 identified by NV05;
+grant create session to NV06 identified by NV06;
+grant create session to NV07 identified by NV07;
+grant create session to NV08 identified by NV08;
+grant create session to NV09 identified by NV09;
+grant create session to NV10 identified by NV10;
+grant create session to NV11 identified by NV11;
+grant create session to NV12 identified by NV12;
+grant create session to NV13 identified by NV13;
+grant create session to NV14 identified by NV14;
+grant create session to NV15 identified by NV15;
+grant create session to NV16 identified by NV16;
+grant create session to NV17 identified by NV17;
+grant create session to NV18 identified by NV18;
+grant create session to NV19 identified by NV19;
+grant create session to NV20 identified by NV20;
+grant create session to NV21 identified by NV21;
+grant create session to NV22 identified by NV22;
+grant create session to NV23 identified by NV23;
+grant create session to NV24 identified by NV24;
+grant create session to NV25 identified by NV25;
+grant create session to NV26 identified by NV26;
+grant create session to NV27 identified by NV27;
+grant create session to NV28 identified by NV28;
+grant create session to NV29 identified by NV29;
+grant create session to NV30 identified by NV30;
+grant create session to NV31 identified by NV31;
+grant create session to NV32 identified by NV32;
+grant create session to NV33 identified by NV33;
+grant create session to NV34 identified by NV34;
+grant create session to NV35 identified by NV35;
+grant create session to NV36 identified by NV36;
+
+-- Create user  for ProjectManager
+grant create session to PM01 identified by PM01;
+grant create session to PM02 identified by PM02;
+grant create session to PM03 identified by PM03;
+grant create session to PM04 identified by PM04;
+grant create session to PM05 identified by PM05;
+
+-- Create user for DepartmentChief
+grant create session to PM01 identified by PM01;
+grant create session to PM02 identified by PM02;
+grant create session to PM03 identified by PM03;
+grant create session to PM04 identified by PM04;
+grant create session to PM05 identified by PM05;
+
+-- Create user for BarnchManager
+grant create session to BM01 identified by BM01;
+grant create session to BM02 identified by BM02;
+grant create session to BM03 identified by BM03;
+grant create session to BM04 identified by BM04;
+grant create session to BM05 identified by BM05;
+
+-- Create user for Director
+grant create session to D01 identified by D01;
+grant create session to D02 identified by D02;
+grant create session to D03 identified by D03;
+grant create session to D04 identified by D04;
+grant create session to D05 identified by D05;
