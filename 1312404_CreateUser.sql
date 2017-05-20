72571e0da708ba67d6ae1a224ed588bbe0eddaba
Thái Quỳnh Nhi <quynhnhi220895@gmail.com>
2017-05-20 00:00:51 -0700

    CreateUser.sql Edting
    Edits file name and script

---
+++ 1312404_CreateUser.sql
@@ -0,0 +1,65 @@
+-- Create user for Employee
+create user nv01 identified by nv01;
+create user nv02 identified by nv02;
+create user nv03 identified by nv03;
+create user nv04 identified by nv04;
+create user nv05 identified by nv05;
+create user nv06 identified by nv06;
+create user nv07 identified by nv07;
+create user nv08 identified by nv08;
+create user nv09 identified by nv09;
+create user nv10 identified by nv10;
+create user nv11 identified by nv11;
+create user nv12 identified by nv12;
+create user nv13 identified by nv13;
+create user nv14 identified by nv14;
+create user nv15 identified by nv15;
+create user nv16 identified by nv16;
+create user nv17 identified by nv17;
+create user nv18 identified by nv18;
+create user nv19 identified by nv19;
+create user nv20 identified by nv20;
+create user nv21 identified by nv21;
+create user nv22 identified by nv22;
+create user nv23 identified by nv23;
+create user nv24 identified by nv24;
+create user nv25 identified by nv25;
+create user nv26 identified by nv26;
+create user nv27 identified by nv27;
+create user nv28 identified by nv28;
+create user nv29 identified by nv29;
+create user nv30 identified by nv30;
+create user nv31 identified by nv31;
+create user nv32 identified by nv32;
+create user nv33 identified by nv33;
+create user nv34 identified by nv34;
+create user nv35 identified by nv35;
+create user nv36 identified by nv36;
+
+-- Create user  for ProjectManager
+create user pm01 identified by pm01;
+create user pm02 identified by pm02;
+create user pm03 identified by pm03;
+create user pm04 identified by pm04;
+create user pm05 identified by pm05;
+
+-- Create user for DepartmentChief
+create user dc01 identified by dc01;
+create user dc02 identified by dc02;
+create user dc03 identified by dc03;
+create user dc04 identified by dc04;
+create user dc05 identified by dc05;
+
+-- Create user for BarnchManager
+create user bm01 identified by bm01;
+create user bm02 identified by bm02;
+create user bm03 identified by bm03;
+create user bm04 identified by bm04;
+create user bm05 identified by bm05;
+
+-- Create user for Director
+create user d01 identified by d01;
+create user d02 identified by d02;
+create user d03 identified by d03;
+create user d04 identified by d04;
+create user d05 identified by d05;
