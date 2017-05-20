5024b79663537738f687fe1eb3becfab9eb1d17d
1412105 <1412105@student.hcmus.edu.vn>
2017-05-20 17:49:32 +0700

    Update DAC complete request DAC 2

--- DAC.sql
+++ DAC.sql
@@ -1,2 +1,20 @@
--- Only Department Chief can insert and update to Projects
+------ Only Department Chief can insert and update to Projects (1312404)
 grant insert, update on Projects to DepartmentChief;
+
+
+------ Director can select on Project(ProjectID, ProjectName, HoldingDepartment, BrandName, PojectManagerName and Fund) (DAC). (1412105)
+
+-- Create view according to request
+CREATE VIEW Projects_DirectorView AS
+SELECT Projects.ProjectID, Projects.ProjectName, Department.DepartmentName, Branch.BranchName, Employee.EmployeeName, Projects.FUND
+FROM Projects
+left outer join Department
+ON Department.DepartmentID = Projects.HoldingDepartment and Department.Branch = Projects.Branch
+left outer join Branch
+ON Branch.BranchID = Projects.Branch
+left outer join Employee
+ON Employee.EmployeeID = Projects.ProjectManager;
+
+-- grant View to Role
+grant select on Projects_DirectorView to Director;
+
